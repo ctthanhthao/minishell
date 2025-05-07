b0eb782b94945b0ad4fffc843116a25d28b39037
ctthanhthao <ctthanhthao@gmail.com>
2025-05-07 14:38:59 +0200

    add execute_builtin

--- executor/execute_builtin.c
+++ executor/execute_builtin.c
@@ -6,21 +6,49 @@
 /*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:42:30 by thchau            #+#    #+#             */
-/*   Updated: 2025/05/06 13:54:38 by thchau           ###   ########.fr       */
+/*   Updated: 2025/05/07 13:29:10 by thchau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int is_builtin(const char *cmd)
+int	is_builtin(const char *cmd)
 {
-    return (!ft_memcmp(cmd, "cd", 2) ||
-            !ft_memcmp(cmd, "exit", 4) ||
-            !ft_memcmp(cmd, "export", 6) ||
-            !ft_memcmp(cmd, "unset", 5) ||
-            !ft_memcmp(cmd, "echo", 4) ||
-            !ft_memcmp(cmd, "env", 3) ||
-            !ft_memcmp(cmd, "pwd", 3));     
+	return (!ft_memcmp(cmd, "cd", 2) ||
+			!ft_memcmp(cmd, "exit", 4) ||
+			!ft_memcmp(cmd, "export", 6) ||
+			!ft_memcmp(cmd, "unset", 5) ||
+			!ft_memcmp(cmd, "echo", 4) ||
+			!ft_memcmp(cmd, "env", 3) ||
+			!ft_memcmp(cmd, "pwd", 3));
 }
 
-int execute_builtin(t_cmd *cmd, char )
+static int	unset_builtin(t_cmd *cmd, char ***envp)
+{
+	// Implementation of unset builtin
+	return (0);
+}
+
+static int	env_builtin(t_cmd *cmd, char ***envp)
+{
+	// Implementation of env builtin
+	return (0);
+}
+
+int execute_builtin(t_cmd *cmd, char ***envp)
+{
+	if (!ft_memcmp(cmd->argv[0], "cd", 2))
+		return (cd_builtin(cmd));
+	else if (!ft_memcmp(cmd->argv[0], "exit", 4))
+		exit(0);
+	else if (!ft_memcmp(cmd->argv[0], "export", 6))
+		return (export_builtin(cmd, envp));
+	else if (!ft_memcmp(cmd->argv[0], "unset", 5))
+		return (unset_builtin(cmd, envp));
+	else if (!ft_memcmp(cmd->argv[0], "echo", 4))
+		return (echo_builtin(cmd, envp));
+	else if (!ft_memcmp(cmd->argv[0], "env", 3))
+		return (env_builtin(cmd, envp));
+	else if (!ft_memcmp(cmd->argv[0], "pwd", 3))
+		return (pwd_builtin(cmd));
+}
--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amarcz <amarcz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 11:35:36 by thchau            #+#    #+#             */
-/*   Updated: 2025/05/07 13:14:51 by amarcz           ###   ########.fr       */
+/*   Updated: 2025/05/07 14:40:04 by thchau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,23 @@
 // TOKENS - DUDE:
 
 typedef enum e_cmd_type {
-    CMD_NONE,
-    CMD_AND_IF,
-    CMD_OR_IF,
-    CMD_PIPE
+	CMD_NONE,
+	CMD_AND_IF,
+	CMD_OR_IF,
+	CMD_PIPE
 }   t_cmd_type;
 
 typedef enum e_token
 {
-    CMD, //A command: ls, echo, cd
-    WORD, //A generic word arguement
-    PIPE, // |
-    MORE, // >
-    MOREMORE, // >>
-    LESS, // <
-    LESSLESS, // <<
-    AND, // &&
-    OR // ||
+	CMD, //A command: ls, echo, cd
+	WORD, //A generic word arguement
+	PIPE, // |
+	MORE, // >
+	MOREMORE, // >>
+	LESS, // <
+	LESSLESS, // <<
+	AND, // &&
+	OR // ||
 }   t_token;
 
 // ===============================
@@ -52,17 +52,17 @@ typedef enum e_token
 // ===============================
 typedef struct s_redir
 {
-    t_token     type;
-    char            *filename;  // or delimiter for heredoc
-    struct s_redir  *next;
+	t_token     type;
+	char            *filename;  // or delimiter for heredoc
+	struct s_redir  *next;
 }   t_redir;
 
 typedef struct s_cmd
 {
-    char          **argv;       // arguments passed to execve or builtin
-    t_redir        *redirs;     // list of redirections
-    struct s_cmd   *next;       // next command in pipeline
-    t_cmd_type     next_type; // e.g. AND_IF, OR_IF, PIPE
+	char          **argv;       // arguments passed to execve or builtin
+	t_redir        *redirs;     // list of redirections
+	struct s_cmd   *next;       // next command in pipeline
+	t_cmd_type     next_type; // e.g. AND_IF, OR_IF, PIPE
 }   t_cmd;
 
 // ===============================
@@ -82,11 +82,14 @@ t_redir *parse_redirections(char **tokens, int *i);                  // optional
 // ===============================
 // EXECUTOR INTERFACE - Thao
 // ===============================
-int     execute_commands(t_cmd *cmd_list, char **envp);
-int     is_builtin(const char *cmd);
-int     execute_builtin(t_cmd *cmd, char ***envp);
-void    apply_redirections(t_redir *redirs);
-
+int		execute_commands(t_cmd *cmd_list, char **envp);
+int		is_builtin(const char *cmd);
+int		execute_builtin(t_cmd *cmd, char ***envp);
+void	apply_redirections(t_redir *redirs);
+int		cd_builtin(t_cmd *cmd);
+int		pwd_builtin(t_cmd *cmd);
+int		export_builtin(t_cmd *cmd, char ***envp);
+int		echo_builtin(t_cmd *cmd);
 // ===============================
 // CLEANUP / UTILS
 // ===============================
