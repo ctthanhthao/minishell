8634ae37d27114b8c99beda0d14af0282823628f
ctthanhthao <ctthanhthao@gmail.com>
2025-05-19 15:33:38 +0200

    reverse to Andras's version

--- input/parse.c
+++ input/parse.c
@@ -6,7 +6,7 @@
 /*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:36:53 by amarcz            #+#    #+#             */
-/*   Updated: 2025/05/19 15:25:50 by thchau           ###   ########.fr       */
+/*   Updated: 2025/05/19 15:32:04 by thchau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ t_redir *add_token(t_redir *last, char *value)
 }
 
 //Main parsing function:
-t_cmd *parse_input(char *input, char **env)
+t_cmd *parse_input(char *input)
 {
     char **tokens;
     int i;
@@ -146,7 +146,7 @@ t_cmd *parse_input(char *input, char **env)
             continue;
         }
         //ft_printf("Rest:\n");
-        expand_dollar(NULL, 0, env);
+        //expand_dollar(NULL, 0, env);
         curr->argv[argv_i++] = ft_strdup(tokens[i++]);// 
         // new = malloc(sizeof(t_redir));
         // if (!new)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:11:32 by amarcz            #+#    #+#             */
-/*   Updated: 2025/05/19 14:53:29 by thchau           ###   ########.fr       */
+/*   Updated: 2025/05/19 15:32:20 by thchau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main (int argc, char **argv, char **envp)
     char *input;
     t_cmd *cmd;
     char    **shell_envp;
-    int status = 0;
+//    int status = 0;
     (void)argc;
 	(void)argv;
 //    int i;
@@ -38,14 +38,14 @@ int main (int argc, char **argv, char **envp)
         add_history(input);
 
         //Parse the input
-        cmd = parse_input(input, shell_envp);
+        cmd = parse_input(input);
         // if (apply_redirections(cmd->redirs) == CMD_FAILURE)
 		// {
 		// 	status = 1;
 		// 	free_cmd(cmd);
 		// 	continue;
 		// }
-        execute_commands(cmd, shell_envp, &status);
+       // execute_commands(cmd, shell_envp, &status);
         
         print_cmds(cmd); //Debug output
         free_cmds(cmd);
@@ -53,5 +53,6 @@ int main (int argc, char **argv, char **envp)
         //free the memory allocated in readline
         free(input);
     }
+    free_split(shell_envp);
     return (0);
 }
