51e7b6a162b547c948a593ea22b4aa2862905e79
Andras Marcz <amarcz@c2r4s1.42prague.com>
2025-06-06 12:25:08 +0200

    main.c norminette add ons

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: amarcz <amarcz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:11:32 by amarcz            #+#    #+#             */
-/*   Updated: 2025/06/06 12:07:39 by amarcz           ###   ########.fr       */
+/*   Updated: 2025/06/06 12:24:07 by amarcz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
 
-int	minishell_loop(char **shell_envp, int *last_status)
+int	minishell_loop(char ***shell_envp, int *last_status)
 {
 	char	*input;
 	t_cmd	*cmd;
@@ -24,7 +24,7 @@ int	minishell_loop(char **shell_envp, int *last_status)
 			break ;
 		if (*input)
 			add_history(input);
-		cmd = parse_input(input, last_status, shell_envp);
+		cmd = parse_input(input, *last_status, *shell_envp);
 		free(input);
 		if (!cmd)
 		{
@@ -32,7 +32,7 @@ int	minishell_loop(char **shell_envp, int *last_status)
 			continue ;
 		}
 		if (!cmd->argv && cmd->redirs)
-			*last_status = handle_builtin_with_redirection(cmd, &shell_envp,
+			*last_status = handle_builtin_with_redirection(cmd, shell_envp,
 					last_status, NULL);
 		else
 			*last_status = execute_commands(cmd, shell_envp, last_status);
@@ -43,10 +43,9 @@ int	minishell_loop(char **shell_envp, int *last_status)
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*input;
-	t_cmd	*cmd;
 	char	**shell_envp;
 	int		last_status;
+	int		ret;
 
 	(void)argc;
 	(void)argv;
@@ -55,7 +54,7 @@ int	main(int argc, char **argv, char **envp)
 	if (!shell_envp)
 		return (1);
 	setup_signals();
-	minishell_loop(&shell_envp, &last_status);
+	ret = minishell_loop(&shell_envp, &last_status);
 	free_split(shell_envp);
-	return (0);
+	return (ret);
 }
