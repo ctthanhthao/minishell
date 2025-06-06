de3f09e5042f07d745c469031e03d18bc2c3eaf9
Andras Marcz <amarcz@c2r4s1.42prague.com>
2025-06-06 12:10:09 +0200

    normientte main.c + minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amarcz <amarcz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 11:35:36 by thchau            #+#    #+#             */
-/*   Updated: 2025/06/05 15:16:30 by amarcz           ###   ########.fr       */
+/*   Updated: 2025/06/06 11:29:41 by amarcz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,23 @@
 
 # define RST		"\033[0m" // Reset formatting
 # define R			"\033[31m" // Red
-#define BLUE		"\001\033[1;34m\002"
-#define RESET		"\001\033[0m\002"
+# define BLUE		"\001\033[1;34m\002"
+# define RESET		"\001\033[0m\002"
 
-# define TOKENIZE_ERROR ((char **)-1)
+//# define TOKENIZE_ERROR ((char **)-1)
 
 // ===============================
 // ENUMS
 // ===============================
 // TOKENS - DUDE:
 
-typedef enum e_cmd_type {
+typedef enum e_cmd_type
+{
 	CMD_NONE,
 	CMD_AND_IF,
 	CMD_OR_IF,
 	CMD_PIPE
-}   t_cmd_type;
+}	t_cmd_type;
 
 typedef enum e_token
 {
@@ -60,23 +61,22 @@ typedef enum e_token
 	REDIR_IN, // <
 	REDIR_HEREDOC, // <<
 	AND, // &&
-	OR // ||
-}   t_token;
+	OR
+}	t_token;
 
 typedef enum e_cmd_status
 {
 	CMD_EXIT = -1,
 	CMD_SUCCESS = 0,
 	CMD_FAILURE = 1
-}   t_cmd_status;
-
+}	t_cmd_status;
 // ===============================
 // STRUCTS
 // ===============================
 typedef struct s_redir
 {
 	t_token			type;
-	char			*filename;  // or delimiter for heredoc
+	char			*filename;
 	struct s_redir	*next;
 }	t_redir;
 
@@ -95,7 +95,7 @@ typedef struct s_bufinfo
 	int		capacity;
 }	t_bufinfo;
 
-typedef	struct s_parse_state
+typedef struct s_parse_state
 {
 	int			i;
 	int			argv_i;
@@ -104,8 +104,7 @@ typedef	struct s_parse_state
 	t_cmd		*prev;
 	int			last_status;
 	char		**envp;
-} 	t_parse_state;
-
+}	t_parse_state;
 
 typedef struct s_pid_pipe_fd
 {
@@ -117,29 +116,35 @@ typedef struct s_pid_pipe_fd
 }	t_pid_pipe_fd;
 
 // ===============================
-// PARSER INTERFACE - András
+// INT
 // ===============================
-t_cmd	*parse_input(char *input, int last_status, char **envp);
+
+typedef int	(*t_builtin_op)(t_cmd*, char***, int*);
+
+// ===============================
+// PARSER INTERFACE - Andras
+// ===============================
 t_token	identify_tok(char *str);
+t_cmd	*parse_input(char *input, int last_status, char **envp);
 int		handle_token(char **tokens, t_parse_state *s);
 int		wildcard_check(char **tokens);
 int		check_invalid_chars(const char *input);
 int		check_unclosed_quotes(const char *input);
 char	*complete_input(void);
 int		is_logical_op(char *token);
-void    print_cmds(t_cmd *cmd);
-char    **ft_tokenize(char *input);
+void	print_cmds(t_cmd *cmd);
+char	**ft_tokenize(char *input);
 int		skip_whitespace(const char *input, int i);
 void	token_ender(char **tokens, int tokeni);
-int     is_redirection(char *token);
-int    handle_redirection(t_cmd *cmd, char **tokens, int *i);
+int		is_redirection(char *token);
+int		handle_redirection(t_cmd *cmd, char **tokens, int *i);
 int		validate_tokens(char **tokens);
 int		is_quoted(char *token);
-void    free_split(char **arr);
-char    **tokenize_line(const char *line);                            // optional internal
-t_cmd   *build_cmd_list(char **tokens, char **envp);                 // optional internal
-t_redir *parse_redirections(char **tokens, int *i);  
-char	**handle_expansion_if_any(char *arg, int last_status, char **env); 
+void	free_split(char **arr);
+char	**tokenize_line(const char *line);
+t_cmd	*build_cmd_list(char **tokens, char **envp);
+t_redir	*parse_redirections(char **tokens, int *i);
+char	**handle_expansion_if_any(char *arg, int last_status, char **env);
 char	*expand_variables(const char *arg, int last_status, char **env);
 char	*expand_one_var(char **p, int last_status, char **env);
 void	setup_signals(void);
@@ -157,21 +162,21 @@ int		is_builtin(const char *cmd);
 bool	has_file_arguments(t_cmd *cmd);
 int		execute_builtin(t_cmd *cmd, char ***envp, int *status);
 int		handle_builtin_with_redirection(t_cmd *cmd, char ***envp, int *status,
-	int (*operation)(t_cmd*, char***, int*));
+			int (*operation)(t_cmd*, char***, int*));
 int		process_pipe(t_cmd *cmd, char ***envp, int *last_status);
 int		apply_redirections(t_redir *redirs, int last_status, char **envp);
 bool	save_original_std_inout(int *stdin_bk, int *stdout_bk);
 void	restore_original_std_inout(int stdin_bk, int stdout_bk);
 int		process_heredoc(t_redir *redir);
 int		cd_builtin(t_cmd *cmd);
-int		pwd_builtin();
+int		pwd_builtin(void);
 int		export_builtin(t_cmd *cmd, char ***envp);
 int		echo_builtin(t_cmd *cmd, int *status);
 int		unset_builtin(t_cmd *cmd, char ***envp);
 int		env_builtin(char **envp);
-int 	exit_builtin(t_cmd *cmd, char ***envp);
+int		exit_builtin(t_cmd *cmd, char ***envp);
 int		execute_single_command(t_cmd *cmd, char ***envp, int *last_status,
-		bool should_fork);
+			bool should_fork);
 
 // ===============================
 // CLEANUP / UTILS
@@ -190,7 +195,7 @@ void	print_sorted_env(char **env);
 int		safe_dup2(int oldfd, int newfd, char *error);
 char	*extract_key(const char *entry);
 char	*strip_quotes(const char *str);
-int		return_failed_exit_code();
+int		return_failed_exit_code(void);
 char	*remove_quotes_if_need(char *arg);
 
 #endif
--- main.c
+++ main.c
@@ -3,62 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thchau <thchau@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: amarcz <amarcz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 14:11:32 by amarcz            #+#    #+#             */
-/*   Updated: 2025/05/31 20:40:42 by thchau           ###   ########.fr       */
+/*   Updated: 2025/06/06 12:07:39 by amarcz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
 
-int main (int argc, char **argv, char **envp)
+int	minishell_loop(char **shell_envp, int *last_status)
 {
-    char *input;
-    t_cmd *cmd;
-    char    **shell_envp;
-	int		last_status = 0;
-//    int status = 0;
-    (void)argc;
-	(void)argv;
-//    int i;
+	char	*input;
+	t_cmd	*cmd;
 
-//    i = -1;
-    //MAIN LOOP
-    shell_envp = clone_arr(envp);
-	if (!shell_envp)
-		return (1);
-	setup_signals();
-    while (1)
-    {
-        //DISPLAY THE PROMPT
-        input = complete_input();
-
-        //Exit the shellif user presses CTRL+D (EOF)
-        if(!input)
-            break;
-        //add input to history
+	while (1)
+	{
+		input = complete_input();
+		if (!input)
+			break ;
 		if (*input)
-        	add_history(input);
-
-        //Parse the input
-        cmd = parse_input(input, last_status, shell_envp);
-		//free the memory allocated in readline
-        free(input);
+			add_history(input);
+		cmd = parse_input(input, last_status, shell_envp);
+		free(input);
 		if (!cmd)
 		{
 			free_cmd(cmd);
-			continue;
+			continue ;
 		}
 		if (!cmd->argv && cmd->redirs)
-			last_status = handle_builtin_with_redirection(cmd, &shell_envp,
-							&last_status, NULL);
+			*last_status = handle_builtin_with_redirection(cmd, &shell_envp,
+					last_status, NULL);
 		else
-			last_status = execute_commands(cmd, &shell_envp, &last_status);
-		//print_cmds(cmd); //Debug output
-        free_cmd(cmd);
+			*last_status = execute_commands(cmd, shell_envp, last_status);
+		free_cmd(cmd);
 	}
+	return (0);
+}
 
-    free_split(shell_envp);
-    return (0);
+int	main(int argc, char **argv, char **envp)
+{
+	char	*input;
+	t_cmd	*cmd;
+	char	**shell_envp;
+	int		last_status;
+
+	(void)argc;
+	(void)argv;
+	last_status = 0;
+	shell_envp = clone_arr(envp);
+	if (!shell_envp)
+		return (1);
+	setup_signals();
+	minishell_loop(&shell_envp, &last_status);
+	free_split(shell_envp);
+	return (0);
 }
